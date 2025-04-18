ad25b36125ca5cce94d828aba365535d54af2439
Alois117 <aloismbithi01@gmail.com>
2025-04-18 19:08:00 +0300

    Update and rename week1.py to main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from pet import Pet
+
+# Create your pet
+buddy = Pet("Buddy")
+
+# Interact with the pet
+buddy.get_status()
+buddy.eat()
+buddy.sleep()
+buddy.play()
+buddy.get_status()
+
+# Teach and display tricks
+buddy.train("roll over")
+buddy.train("play dead")
+buddy.show_tricks()
+
