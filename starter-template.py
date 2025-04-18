78d5096986d961e669facb9025bb912b14827e00
Alois117 <aloismbithi01@gmail.com>
2025-04-18 19:06:48 +0300

    pet class answer update

--- starter-template.py
+++ starter-template.py
@@ -7,19 +7,34 @@ class Pet:
         self.tricks = []
 
     def eat(self):
-        # TODO
+        self.hunger = max(0, self.hunger - 3)
+        self.happiness = min(10, self.happiness + 1)
+        print(f"{self.name} ate some food.")
 
     def sleep(self):
-        # TODO
+        self.energy = min(10, self.energy + 5)
+        print(f"{self.name} took a nap.")
 
     def play(self):
-        # TODO
-
-    def train(self, trick):
-        # TODO
-
-    def show_tricks(self):
-        # TODO
+        if self.energy >= 2:
+            self.energy -= 2
+            self.happiness = min(10, self.happiness + 2)
+            self.hunger = min(10, self.hunger + 1)
+            print(f"{self.name} played happily!")
+        else:
+            print(f"{self.name} is too tired to play.")
 
     def get_status(self):
-        # TODO
+        print(f"🐾 {self.name}'s Status:")
+        print(f"Hunger: {self.hunger}/10")
+        print(f"Energy: {self.energy}/10")
+        print(f"Happiness: {self.happiness}/10")
+        print()
+
+    def train(self, trick):
+        self.tricks.append(trick)
+        self.happiness = min(10, self.happiness + 1)
+        print(f"{self.name} learned a new trick: {trick}!")
+
+    def show_tricks(self):
+        print(f"{self.name}'s Tricks: {', '.join(self.tricks) if self.tricks else 'None yet.'}")
